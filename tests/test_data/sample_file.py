11b63f9539edc83d3cb682fbaae9be07c37db352
Noor Buchi <buchin@allegheny.edu>
2022-03-27 21:55:16 -0400

    add sample file for testin

---
+++ tests/test_data/sample_file.py
@@ -0,0 +1,22 @@
+"""Use module is used for testing purposes only, do not modify."""
+
+
+def some_function(arg1, arg2, arg3, arg4):
+    print("hello world!")
+    int1 = arg1 * arg3 / 5 - arg4
+    int2 = arg4 ** arg3
+    my_list = [1, 2, 3.6, 4, 5.4, "some string"]
+    my_dictionary = {
+        "first": arg1,
+        "second": arg2,
+        "third": arg3,
+        "fourth": arg4,
+    }
+    my_tuple = ("hello", "world")
+    result = some_other_function(arg2).process_somehow()
+    if not arg3 > arg2 and arg1 < arg4:
+        return arg2 >> 5
+    while my_list[4] > my_dictionary["first"]:
+        my_variable = 54
+        my_variable += arg1 / arg4
+    return my_dictionary["first"] + "something here"
